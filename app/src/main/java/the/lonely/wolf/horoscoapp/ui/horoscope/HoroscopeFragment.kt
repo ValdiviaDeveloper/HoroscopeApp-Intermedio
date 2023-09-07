e37d52295ef9de331cdfe78f75da06d977c432ec
TheLonelyWolf <valdivia.developer@gmail.com>
2023-09-07 16:35:50 -0500

    Initial commit

---
+++ app/src/main/java/the/lonely/wolf/horoscoapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,26 @@
+package the.lonely.wolf.horoscoapp.ui.horoscope
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import the.lonely.wolf.horoscoapp.R
+import the.lonely.wolf.horoscoapp.databinding.FragmentHoroscopeBinding
+
+
+class HoroscopeFragment : Fragment() {
+
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+}
